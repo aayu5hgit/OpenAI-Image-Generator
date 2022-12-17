4e7f3fcb799c5659005a9a28016ffc0004a83c28
AAYUSH TALREJA <86314754+aayu5hgit@users.noreply.github.com>
2022-12-17 19:35:09 +0530

    logo + alert

--- OpenAI Image Generator/ContentView.swift
+++ OpenAI Image Generator/ContentView.swift
@@ -7,6 +7,8 @@
 
 import OpenAIKit
 import SwiftUI
+import SimpleToast
+
 
 final class ViewModel: ObservableObject{
     private var openai: OpenAI?
@@ -14,7 +16,8 @@ final class ViewModel: ObservableObject{
     final func setup(){
         openai = OpenAI(Configuration(
             organization: "Personal",
-            apiKey: "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"))
+//            apiKey: "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"))
+    apiKey: "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"))
     }
     
     func generateImage(prompt: String) async -> UIImage?{
@@ -38,11 +41,14 @@ final class ViewModel: ObservableObject{
 }
 
 struct ContentView: View {
+    @State private var showingAlert = false
     @ObservedObject var viewModel = ViewModel()
     @State var text = ""
     @State var image: UIImage?
     var body: some View {
+        
         NavigationView {
+            
             VStack {
                 Spacer()
                 if let image = image{
@@ -51,25 +57,41 @@ struct ContentView: View {
                         .aspectRatio(contentMode: .fit)
                         .scaledToFit()
                         .frame(width: 320, height: 320)
+                    
                 } else{
                     Text("Image will appear here!")
+                    
                 }
                 Spacer()
                 
                 TextField("Type Prompt here", text: $text)
                     .padding()
                 Button("Generate"){
-                    
+                    showingAlert = true
                     if !text.trimmingCharacters(in: .whitespaces).isEmpty{
                         Task{
+                            
                             let result = await viewModel.generateImage(prompt: text)
                             if result == nil {
                                 print("Failed to get Image...")
                             }
                             self.image = result
+                            
+                               
+//                            HStack{
+//                                Text("Fetching...").bold()
+//                            }
+//                            .padding(20)
+//                            .background(Color.gray.opacity(0.7))
+//                            .foregroundColor(Color.white)
+//                            .cornerRadius(10)
+//
                         }
                     }
                 }
+                .alert("Fetching can take few seconds...", isPresented: $showingAlert) {
+                           Button("Ok", role: .cancel) { }
+                       }
             }
             
             .navigationTitle("OPEN-AI + DALLE-E")
